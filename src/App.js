8185b38be8d263b7927b76031d662a8084dd8677
GabrielaTiago <gabrielatiagodearaujo@outlook.com>
2022-07-03 19:36:33 -0300

    Feat: Creation of the first routes of the project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import SignIn from "./Pages/SignIn/SignIn";
+import SignUp from "./Pages/SignUp/SignUp";
+
+export default function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<SignIn />}></Route>
+                <Route path="/sign-up" element={<SignUp />}></Route>
+            </Routes>
+        </BrowserRouter>
+    );
+}
