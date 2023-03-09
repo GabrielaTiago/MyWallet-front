3d7cd60c71f9b350b522d1710b9bed0fddee959f
GabsTiago <gabrielatiagodearaujo@outlook.com>
2023-03-08 21:31:45 -0300

    Feat: Creates the service function to handle de response of sign ip

--- src/shared/services/auth/auth.js
+++ src/shared/services/auth/auth.js
@@ -2,6 +2,10 @@ import api from "../api";
 
 export function signUp(body) {
   const response = api.post("/sign-up", body);
+  return response;
+}
 
+export function signIn(body) {
+  const response = api.post("/sign-in", body);
   return response;
 }
