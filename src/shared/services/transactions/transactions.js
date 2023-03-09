340863b1f2d944f524a0d5b97aa5be52213ab8af
GabsTiago <gabrielatiagodearaujo@outlook.com>
2023-03-09 12:36:35 -0300

    Feat: Creates the service function to generate a new transaction

--- src/shared/services/transactions/transactions.js
+++ src/shared/services/transactions/transactions.js
@@ -3,3 +3,7 @@ import api from "../api";
 export function getTransactions(token) {
   return api.get("/transactions", null, token);
 }
+
+export function createTransaction(body, token) {
+  return api.post("/transactions", body, token);
+}
