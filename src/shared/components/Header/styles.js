657d8b85f90b148af2145042b2afb8e9aea0ea13
GabsTiago <gabrielatiagodearaujo@outlook.com>
2023-03-09 23:13:21 -0300

    Refactor: Changes in the 'Header' component

---
+++ src/shared/components/Header/styles.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const Container = styled.header`
+  width: 100%;
+  min-height: 55px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  ion-icon {
+    width: 23px;
+    height: 24px;
+    color: #ffffff;
+  }
+`;
+
+export { Container };
