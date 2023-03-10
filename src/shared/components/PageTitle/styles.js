4e2835669bd4520b99fcf400cac92b6463dd26fc
GabsTiago <gabrielatiagodearaujo@outlook.com>
2023-03-09 23:15:56 -0300

    Feat: Creates the 'PageTitle' component

---
+++ src/shared/components/PageTitle/styles.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const Typography = styled.h2`
+  font-weight: 700;
+  font-size: 26px;
+  line-height: 31px;
+  color: #ffffff;
+`;
+
+export { Typography };
