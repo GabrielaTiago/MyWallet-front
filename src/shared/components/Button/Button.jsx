5b21f61628e72e94e714c713fec9f3a9c321dc0f
GabsTiago <gabrielatiagodearaujo@outlook.com>
2023-03-12 20:07:56 -0300

    Feat: Adds loading to the button

--- src/shared/components/Button/Button.jsx
+++ src/shared/components/Button/Button.jsx
@@ -1,9 +1,10 @@
+import { ThreeDots } from "react-loader-spinner";
 import { ButtonBox } from "./styles";
 
 export function Button({ type, disabled, text }) {
   return (
     <ButtonBox type={type} disabled={disabled}>
-      {text}
+      {disabled ? <ThreeDots color="#ffffff"  height={25} width={55} /> : text}
     </ButtonBox>
   );
 }
