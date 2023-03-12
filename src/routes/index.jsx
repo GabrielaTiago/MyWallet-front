5d111f74b0fd3e4c0f55114fad6c295a861bc42d
GabsTiago <gabrielatiagodearaujo@outlook.com>
2023-03-12 11:51:30 -0300

    Feat: Implements the 'Transaction' page

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,5 +1,5 @@
 import { Navigate, Route, Routes } from "react-router-dom";
-import { Expense, Income, SignIn, SignUp, Wallet } from "../pages";
+import { SignIn, SignUp, Transaction, Wallet } from "../pages";
 
 export function AppRoutes() {
   return (
@@ -7,8 +7,8 @@ export function AppRoutes() {
       <Route path="/" element={<SignIn />}></Route>
       <Route path="/sign-up" element={<SignUp />}></Route>
       <Route path="/wallet" element={<Wallet />}></Route>
-      <Route path="/income" element={<Income />}></Route>
-      <Route path="/expense" element={<Expense />}></Route>
+      <Route path="/transaction" element={<Transaction />}></Route>
+
       <Route path="*" element={<Navigate to={"/"} />} />
     </Routes>
   );
