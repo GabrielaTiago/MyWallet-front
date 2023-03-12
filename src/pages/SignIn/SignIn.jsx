6eb9c60f44d0e7bd39303ff234b7f67ae0132347
GabsTiago <gabrielatiagodearaujo@outlook.com>
2023-03-11 22:20:31 -0300

    Feat: Form manipulation and field validations on the sign in page

--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -1,28 +1,42 @@
-import { useState } from "react";
+import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
+import { isEmail } from "validator";
 import { useUserContext } from "../../shared/contexts";
-import { Button, GoTo, Title } from "../../shared/components";
+import { Button, ErrorMessage, GoTo, Title } from "../../shared/components";
 import { AuthWrapper, FormWrapper } from "../../shared/layout";
 import { signIn } from "../../shared/services";
 
 export function SignIn() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
   const { setUser } = useUserContext();
   const navigate = useNavigate();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
-  function handleSignIn(event) {
-    event.preventDefault();
-    signInUser();
-  }
+  const onSubmit = async (data) => {
+    await signInUser(data);
+  };
 
-  async function signInUser() {
+  async function signInUser({ email, password }) {
     try {
       const response = await signIn({ email, password });
       setUser(response);
+      Swal.fire({
+        icon: "success",
+        title: `${response.message}`,
+        confirmButtonColor: "#8c17be",
+      });
       navigate("/wallet");
     } catch (err) {
-      alert(`${err.data}`);
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: `${err.data}`,
+        confirmButtonColor: "#8c17be",
+      });
     }
   }
 
@@ -31,21 +45,37 @@ export function SignIn() {
       <Title />
 
       <FormWrapper>
-        <form onSubmit={handleSignIn}>
-          <input
-            type="email"
-            placeholder="E-mail"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-          <input
-            type="password"
-            placeholder="Senha"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div>
+            <input
+              className={errors?.email && "input-error"}
+              type="text"
+              placeholder="E-mail"
+              {...register("email", {
+                required: true,
+                validate: (value) => isEmail(value),
+              })}
+            />
+            {errors?.email?.type === "required" && (
+              <ErrorMessage message="O campo email é obrigatório" />
+            )}
+            {errors?.email?.type === "validate" && (
+              <ErrorMessage message="Email inválido" />
+            )}
+          </div>
+
+          <div>
+            <input
+              className={errors?.email && "input-error"}
+              type="password"
+              placeholder="Senha"
+              {...register("password", { required: true })}
+            />
+            {errors?.password?.type === "required" && (
+              <ErrorMessage message="O campo senha é obrigatório" />
+            )}
+          </div>
+
           <Button type="submit" disabled={false} text="Entrar" />
         </form>
       </FormWrapper>
