3cc9f909b6d0139cc79fbcb6e7f377908a1cd539
GabsTiago <gabrielatiagodearaujo@outlook.com>
2023-03-12 17:43:06 -0300

    Feat: Creates the function to delete the transaction

--- src/shared/components/Transaction/Transaction.jsx
+++ src/shared/components/Transaction/Transaction.jsx
@@ -1,4 +1,6 @@
+import { AiOutlineClose } from "react-icons/ai";
 import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
 import {
   Amount,
   Container,
@@ -7,9 +9,14 @@ import {
   LeftBox,
   RightBox,
 } from "./styles";
+import { useUserContext } from "../../contexts";
+import { deleteTransaction } from "../../services";
 
 export function Transaction({ transaction }) {
   const { _id, day, amount, description, type } = transaction;
+  const {
+    user: { token },
+  } = useUserContext();
   const navigate = useNavigate();
   let positive = false;
 
@@ -18,6 +25,37 @@ export function Transaction({ transaction }) {
   const goToEdition = () => {
     navigate("/transaction", {
       state: { type, page: "edit", transactionId: _id },
+    });
+  };
+
+  const deleteUserTransaction = () => {
+    Swal.fire({
+      icon: "warning",
+      title: "Deseja mesmo deletar esta transação?",
+      text: "Você não poderá reverter essa ação...",
+      showCancelButton: true,
+      confirmButtonColor: "#8c17be",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Sim, delete!",
+      cancelButtonText: "Não, cancele!",
+    }).then(async ({ isConfirmed, isDismissed }) => {
+      if (isConfirmed) {
+        await deleteTransaction(token, _id);
+        Swal.fire({
+          icon: "success",
+          title: "Deletado!",
+          text: "Sua transação foi deletada.",
+          confirmButtonColor: "#8c17be",
+        });
+      }
+      if (isDismissed) {
+        Swal.fire({
+          icon: "error",
+          title: "Cancelado",
+          text: "Sua transação está segura :)",
+          confirmButtonColor: "#52B6FF",
+        });
+      }
     });
   };
 
@@ -31,6 +69,7 @@ export function Transaction({ transaction }) {
         <Amount positive={positive} onClick={goToEdition}>
           {amount}
         </Amount>
+        <AiOutlineClose onClick={deleteUserTransaction} />
       </LeftBox>
     </Container>
   );
