4f7f6cae35f63f2729d33d6400665404d36bc2fb
GabsTiago <gabrielatiagodearaujo@outlook.com>
2023-03-12 14:18:41 -0300

    Refactor: Changes transaction request location

--- src/shared/components/AllTransactions/AllTransactions.jsx
+++ src/shared/components/AllTransactions/AllTransactions.jsx
@@ -1,24 +1,54 @@
+import { useCallback, useEffect, useState } from "react";
+import { fire } from "sweetalert2";
 import { Container, Balance, Text, Total, NoContent } from "./styles";
-import { Transaction } from "../Transaction/Transaction";
+import { useUserContext } from "../../contexts";
+import { Transaction } from "../../components";
+import { getTransactions } from "../../services";
 
-export function AllTransactions({ transactions, balance }) {
+export function AllTransactions() {
+  const [listOfTransactions, setListOfTransactions] = useState([]);
+  const [balanceValue, setBalanceValue] = useState(0);
+  const {
+    user: { token },
+  } = useUserContext();
+  const noTransactions = listOfTransactions.length === 0;
   let positive = false;
 
-  if (balance > 0) positive = true;
+  const fetchTransactions = useCallback(async () => {
+    try {
+      const { balance, transactions } = await getTransactions(token);
+
+      setListOfTransactions([...transactions]);
+      setBalanceValue(balance);
+    } catch (err) {
+      fire({
+        icon: "error",
+        title: "Oops...",
+        text: `${err.data}`,
+        confirmButtonColor: "#8c17be",
+      });
+    }
+  }, [token]);
+
+  useEffect(() => {
+    fetchTransactions();
+  }, [fetchTransactions]);
+
+  if (balanceValue > 0) positive = !positive;
 
   return (
     <Container>
-      {transactions.length === 0 ? (
+      {noTransactions ? (
         <NoContent>Não há registros de entrada ou saída</NoContent>
       ) : (
         <>
-          {transactions.map((transaction) => {
+          {listOfTransactions.map((transaction) => {
             const { _id } = transaction;
             return <Transaction key={_id} transaction={transaction} />;
           })}
           <Balance>
             <Text>SALDO</Text>
-            <Total positive={positive}>{balance}</Total>
+            <Total positive={positive}>{balanceValue}</Total>
           </Balance>
         </>
       )}
